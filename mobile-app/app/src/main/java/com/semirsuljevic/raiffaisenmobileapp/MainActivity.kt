f206cfc2ae31cd538e1eeaebffea850e3032641f
semir.suljevic <semir.suljevic@abc-techgroup.com>
2021-12-12 00:29:09 +0100

    added more screen page and ripple theme

--- mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/MainActivity.kt
+++ mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/MainActivity.kt
@@ -3,23 +3,40 @@ package com.semirsuljevic.raiffaisenmobileapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.semirsuljevic.raiffaisenmobileapp.ui.screens.MoreScreen
+import com.semirsuljevic.raiffaisenmobileapp.ui.theme.Grey200
 import com.semirsuljevic.raiffaisenmobileapp.ui.theme.RaiffaisenMobileAppTheme
 import com.semirsuljevic.raiffaisenmobileapp.ui.theme.White
 
+private object JetNewsRippleTheme : RippleTheme {
+    // Here you should return the ripple color you want
+    // and not use the defaultRippleColor extension on RippleTheme.
+    // Using that will override the ripple color set in DarkMode
+    // or when you set light parameter to false
+    @Composable
+    override fun defaultColor(): Color = Color.White
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(
+        Color.White,
+        lightTheme = !isSystemInDarkTheme()
+    )
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +47,18 @@ class MainActivity : ComponentActivity() {
 //                    Greeting("Android")
 //                }
 //            }
-            Scaffold (
-                backgroundColor = Color.Black
+            MaterialTheme(
+
+            ) {
+                CompositionLocalProvider(
+                    LocalRippleTheme provides JetNewsRippleTheme
+                ) {
+                    Scaffold (
+                        backgroundColor = Color.Black
                     ){
-                MoreScreen()
+                        MoreScreen()
+                    }
+                }
             }
         }
     }
@@ -44,14 +69,22 @@ fun RMBListItem(
     title: String,
     icon: ImageVector
 ) {
-    Row {
-        Icon(imageVector = icon, contentDescription = title)
-        Spacer(modifier = Modifier.width(10.dp))
-        Text(
-            text = title,
-            color = White
-        )
+    Box(modifier = Modifier.clickable {
 
+    }) {
+        Row (
+            Modifier.fillMaxWidth().padding(all = 12.dp)
+
+        ){
+            Icon(imageVector = icon, contentDescription = title, tint = Grey200, modifier = Modifier.size(26.dp))
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = title,
+                color = White,
+                fontSize = 17.sp
+            )
+
+        }
     }
 }
 
--- mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/screens/MoreScreen.kt
+++ mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/screens/MoreScreen.kt
@@ -3,10 +3,13 @@ package com.semirsuljevic.raiffaisenmobileapp.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.semirsuljevic.raiffaisenmobileapp.R
+import com.semirsuljevic.raiffaisenmobileapp.RMBListItem
 import com.semirsuljevic.raiffaisenmobileapp.ui.composables.ListSectionSeparator
 import com.semirsuljevic.raiffaisenmobileapp.ui.composables.ScreenTitle
 
@@ -19,5 +22,16 @@ fun MoreScreen() {
     ){
         ScreenTitle(text = stringResource(id = R.string.more_screen_title))
         ListSectionSeparator(text = stringResource(id = R.string.more_screen_section_tools))
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_calculators), icon = Icons.Outlined.Calculate)
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_currency_list), icon = Icons.Outlined.FeaturedPlayList)
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_currency_convertor), icon = Icons.Outlined.Money)
+        ListSectionSeparator(text = stringResource(id = R.string.more_screen_section_about_app))
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_intro_informatio), icon = Icons.Outlined.ContactSupport)
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_faq), icon = Icons.Outlined.Quiz)
+        ListSectionSeparator(text = stringResource(id = R.string.more_screen_section_about_bank))
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_intro_informatio), icon = Icons.Outlined.Quiz)
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_contact), icon = Icons.Outlined.ContactPage)
+        RMBListItem(title = stringResource(id = R.string.more_screen_item_follow_us), icon = Icons.Outlined.Subscriptions)
+
     }
 }
