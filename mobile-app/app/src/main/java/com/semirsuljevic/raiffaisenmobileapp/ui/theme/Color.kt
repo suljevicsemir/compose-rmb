ff66644501c3417928d3c5adad99b002991db0b4
semir.suljevic <semir.suljevic@abc-techgroup.com>
2021-12-11 20:28:49 +0100

    added dark theme colors

--- mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/theme/Color.kt
+++ mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/theme/Color.kt
@@ -6,3 +6,12 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Yellow200 = Color(0xFF44400e)
+val Yellow400 = Color(0xFFf7cf29)
+
+val Grey200 = Color(0xFF9c9a9d)
+val Grey400 = Color(0xFF1a1c19)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
