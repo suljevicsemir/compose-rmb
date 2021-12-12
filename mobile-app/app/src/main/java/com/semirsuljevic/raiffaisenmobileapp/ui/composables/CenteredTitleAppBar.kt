d2a8a0c35995719bdea35101ffe4eef4ab93a45c
semir.suljevic <semir.suljevic@abc-techgroup.com>
2021-12-12 02:29:22 +0100

    added widget manager fixes

---
+++ mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/composables/CenteredTitleAppBar.kt
@@ -0,0 +1,46 @@
+package com.semirsuljevic.raiffaisenmobileapp.ui.composables
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.semirsuljevic.raiffaisenmobileapp.ui.theme.White
+
+@Composable
+fun CenteredTitleAppBar(title: String) {
+    TopAppBar(
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp,
+        modifier= Modifier.fillMaxWidth()
+    ) {
+        Box(Modifier.height(48.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = { },
+                    enabled = true,
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = "Back",
+                        tint = White
+                    )
+                }
+            }
+            Row(
+                Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically) {
+                    ScreenTitle(text = title)
+                }
+            }
+        }
+
+    }
--- mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/screens/WidgetManager.kt
+++ mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/screens/WidgetManager.kt
@@ -1,47 +1,17 @@
 package com.semirsuljevic.raiffaisenmobileapp.ui.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.semirsuljevic.raiffaisenmobileapp.R
+import com.semirsuljevic.raiffaisenmobileapp.ui.composables.CenteredTitleAppBar
 import com.semirsuljevic.raiffaisenmobileapp.ui.composables.ListItemSeparator
 import com.semirsuljevic.raiffaisenmobileapp.ui.composables.RMBListItemSwitch
-import com.semirsuljevic.raiffaisenmobileapp.ui.theme.White
 
 @Composable
 fun WidgetManager() {
     Column {
-        Box (
-            modifier = Modifier.wrapContentSize(align = Alignment.Center)
-                )
-        {
-            IconButton(
-                onClick = {}
-            ) {
-                Icon(imageVector = Icons.Outlined.ArrowBack, contentDescription = "Arrow Back", tint = White)
-            }
-
-            Text(
-                text = stringResource(id = R.string.widget_manager_screen_title),
-                color = White,
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp,
-                modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentSize(align = Alignment.Center)
-                    .padding(top = 8.dp)
-            )
-        }
+        CenteredTitleAppBar(title = stringResource(id = R.string.widget_manager_screen_title))
         ListItemSeparator()
         RMBListItemSwitch(title = stringResource(id = R.string.widget_manager_screen_item_transactions), icon = null) {}
         ListItemSeparator()
