006c317f577c665255dd10e2da31a3104a10ecf3
semir.suljevic <semir.suljevic@abc-techgroup.com>
2022-02-04 02:16:27 +0100

    maps for whatever fucking reason stopped working

--- mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/screens/intro_info/IntroHelp.kt
+++ mobile-app/app/src/main/java/com/semirsuljevic/raiffaisenmobileapp/ui/screens/intro_info/IntroHelp.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
@@ -20,6 +21,7 @@ import com.semirsuljevic.raiffaisenmobileapp.ui.composables.CenteredTitleAppBar
 import com.semirsuljevic.raiffaisenmobileapp.ui.composables.ListItemSeparator
 import com.semirsuljevic.raiffaisenmobileapp.ui.composables.RMBListItem
 import com.semirsuljevic.raiffaisenmobileapp.ui.navigation.Screen
+import com.semirsuljevic.raiffaisenmobileapp.ui.theme.Black
 import com.semirsuljevic.raiffaisenmobileapp.ui.theme.Gray200
 import com.semirsuljevic.raiffaisenmobileapp.ui.theme.White
 
@@ -27,65 +29,73 @@ import com.semirsuljevic.raiffaisenmobileapp.ui.theme.White
 fun IntroHelpScreen(navController: NavController) {
     val scrollState = rememberScrollState()
 
-    Column(Modifier.verticalScroll(scrollState)) {
-        CenteredTitleAppBar(
-            title = stringResource(id = R.string.intro_help_screen_title),
-            navController = navController
-        )
-        Column (Modifier.padding(horizontal = 20.dp)){
-            Text(
-                text = stringResource(id = R.string.intro_help_screen_desc1),
-                color = White,
-                textAlign = TextAlign.Justify
+    Scaffold (
+        backgroundColor = Black,
+        topBar = {
+            CenteredTitleAppBar(
+                title = stringResource(id = R.string.intro_help_screen_title),
+                navController = navController
             )
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(
-                text = stringResource(id = R.string.intro_help_screen_desc2),
-                color = Gray200,
-                textAlign = TextAlign.Justify
-            )
-            Spacer(modifier = Modifier.height(20.dp))
-
         }
-        RMBListItem(
-            title = stringResource(id = R.string.intro_help_screen_login),
-            icon = Icons.Outlined.Login,
-            onPressed = {
-                navController.navigate(Screen.InfoHelpLogin.route)
+    ){
+        Column(Modifier.verticalScroll(scrollState)) {
+
+
+            Column (Modifier.padding(horizontal = 20.dp)){
+                Text(
+                    text = stringResource(id = R.string.intro_help_screen_desc1),
+                    color = White,
+                    textAlign = TextAlign.Justify
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = stringResource(id = R.string.intro_help_screen_desc2),
+                    color = Gray200,
+                    textAlign = TextAlign.Justify
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+
             }
-        )
-        ListItemSeparator()
-        RMBListItem(
-            title = stringResource(id = R.string.intro_help_screen_settings),
-            icon = Icons.Outlined.Settings,
-            onPressed = {
-                navController.navigate(Screen.InfoHelpSettings.route)
-            }
-        )
-        ListItemSeparator()
-        RMBListItem(
-            title = stringResource(id = R.string.intro_help_screen_lang),
-            icon = Icons.Outlined.Translate,
-            onPressed = {
-                navController.navigate(Screen.InfoHelpLanguage.route)
-            }
-        )
-        ListItemSeparator()
-        RMBListItem(
-            title = stringResource(id = R.string.intro_help_screen_security),
-            icon = Icons.Outlined.Security,
-            onPressed = {
-                navController.navigate(Screen.InfoHelpSecurity.route)
-            }
-        )
-        ListItemSeparator()
-        RMBListItem(
-            title = stringResource(id = R.string.intro_help_screen_tehnical),
-            icon = Icons.Outlined.Description,
-            onPressed = {
-                navController.navigate(Screen.InfoHelpDetails.route)
-            }
-        )
-        ListItemSeparator()
+            RMBListItem(
+                title = stringResource(id = R.string.intro_help_screen_login),
+                icon = Icons.Outlined.Login,
+                onPressed = {
+                    navController.navigate(Screen.InfoHelpLogin.route)
+                }
+            )
+            ListItemSeparator()
+            RMBListItem(
+                title = stringResource(id = R.string.intro_help_screen_settings),
+                icon = Icons.Outlined.Settings,
+                onPressed = {
+                    navController.navigate(Screen.InfoHelpSettings.route)
+                }
+            )
+            ListItemSeparator()
+            RMBListItem(
+                title = stringResource(id = R.string.intro_help_screen_lang),
+                icon = Icons.Outlined.Translate,
+                onPressed = {
+                    navController.navigate(Screen.InfoHelpLanguage.route)
+                }
+            )
+            ListItemSeparator()
+            RMBListItem(
+                title = stringResource(id = R.string.intro_help_screen_security),
+                icon = Icons.Outlined.Security,
+                onPressed = {
+                    navController.navigate(Screen.InfoHelpSecurity.route)
+                }
+            )
+            ListItemSeparator()
+            RMBListItem(
+                title = stringResource(id = R.string.intro_help_screen_tehnical),
+                icon = Icons.Outlined.Description,
+                onPressed = {
+                    navController.navigate(Screen.InfoHelpDetails.route)
+                }
+            )
+            ListItemSeparator()
+        }
     }
 }
